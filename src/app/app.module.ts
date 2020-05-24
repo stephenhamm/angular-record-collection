9dcf35a880828b9542aa52fea5213b364a0aa188
Steve Hamm <steve@brightenapparel.com>
2020-05-24 17:25:48 -0300

    Condensed lists into one component.

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,19 +4,17 @@ import { NgModule } from '@angular/core';
 import { AppComponent } from './app.component';
 import { RecordComponent } from './components/record/record.component';
 import { HeaderComponent } from './components/UI/header/header.component';
-import { ArtistListComponent } from './components/artist-list/artist-list.component';
-import { RecordListComponent } from './components/record-list/record-list.component';
 import { AddRecordComponent } from './components/add-record/add-record.component';
 import { NgbModule } from '@ng-bootstrap/ng-bootstrap';
+import { ListComponent } from './components/list/list.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     RecordComponent,
     HeaderComponent,
-    ArtistListComponent,
-    RecordListComponent,
-    AddRecordComponent
+    AddRecordComponent,
+    ListComponent
   ],
   imports: [
     BrowserModule,
